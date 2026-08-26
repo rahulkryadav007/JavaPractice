16a8dd083b9400c3e9dd1f2263f0de1b0b201735
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-08-26 23:27:15 +0530

    Add operators example

---
+++ Java Training/src/operators/OperatorsDemo.java
@@ -0,0 +1,8 @@
+package operators;
+public class OperatorsDemo {
+    public static void main(String[] args) {
+        int a=10,b=3;
+        System.out.println(a+b); System.out.println(a-b); System.out.println(a*b); System.out.println(a/b); System.out.println(a%b);
+        System.out.println(a>b); System.out.println(a==b); System.out.println(a>b && b>0);
+    }
+}
