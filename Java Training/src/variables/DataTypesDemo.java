8405e075f1fa2edb2812b450a5f4dd2c7fde17d6
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-08-26 23:27:11 +0530

    Add variables and data types example

---
+++ Java Training/src/variables/DataTypesDemo.java
@@ -0,0 +1,7 @@
+package variables;
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        int age = 25; double salary = 50000.50; char grade = 'A'; boolean active = true; String name = "Rahul";
+        System.out.println(age); System.out.println(salary); System.out.println(grade); System.out.println(active); System.out.println(name);
+    }
+}
