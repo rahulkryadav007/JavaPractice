d872cfd6ce26aafaf9bad06fa4ad758cbb525355
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-08-26 23:27:23 +0530

    Add arrays example

---
+++ Java Training/src/arrays/ArrayDemo.java
@@ -0,0 +1,9 @@
+package arrays;
+import java.util.Arrays;
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] a={10,20,30,40};
+        for(int x:a) System.out.println(x);
+        System.out.println(Arrays.toString(a));
+    }
+}
