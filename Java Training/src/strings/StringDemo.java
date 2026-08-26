b743d76d814f125c81b586e7bf91460cf14a6b11
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-08-26 23:27:26 +0530

    Add strings example

---
+++ Java Training/src/strings/StringDemo.java
@@ -0,0 +1,7 @@
+package strings;
+public class StringDemo {
+    public static void main(String[] args) {
+        String s="Java Training";
+        System.out.println(s.length()); System.out.println(s.toUpperCase()); System.out.println(s.charAt(0)); System.out.println(s.substring(0,4));
+    }
+}
